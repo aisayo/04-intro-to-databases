cb16fc45d135ded31810c6f84110c4c5c2b1f139
Aysan Isayo <aisayo@icloud.com>
2021-09-22 21:55:17 -0700

    starter code

--- starships/lib/starship.rb
+++ starships/lib/starship.rb
@@ -2,37 +2,22 @@ class Starship
 
     @@all = [] 
 
-    attr_accessor :starship_name, :model, :color
-    attr_reader :id 
+    attr_accessor :name, :model, :color 
 
     def initialize(attributes) 
         attributes.each do |key, value| 
-            if self.respond_to?("#{key.to_s}=")
-                self.send("#{key.to_s}=", value)
-            end 
+            self.send("#{key}=", value) 
         end
+        save
     end
 
     def save 
-        if self.id
-            self.update
-        else 
-            sql = <<-SQL
-                INSERT INTO starships (starship_name, model, color) VALUES (?, ?, ?);
-            SQL
-
-            DB.execute(sql, self.starship_name, self.model, self.color)
-            @id = DB.last_insert_row_id
-        end 
-        self 
+        @@all << self 
     end 
 
     def self.all 
-        array_of_hashes = DB.execute("SELECT * FROM starships")
-        array_of_hashes.collect do |hash|
-          self.new(hash)
-        end
-    end
+        @@all
+    end 
 
     def self.find(name)
         self.all.find do |starship|
@@ -40,28 +25,4 @@ class Starship
         end 
     end 
 
-    def self.create_table 
-        # write a query that creates the table 
-        sql = <<-SQL
-        CREATE TABLE IF NOT EXISTS starships (
-            id INTEGER PRIMARY KEY, 
-            starship_name TEXT,
-            model TEXT,
-            color TEXT
-        );
-        SQL
-        DB.execute(sql)
-    end 
-
-    def update 
-        sql = <<-SQL
-           UPDATE starships SET starship_name = ?, model = ?, color = ? WHERE id = ?
-        SQL
-
-        DB.execute(sql, self.starship_name, self.model, self.color, self.id)
-        self
-    end
-
 end 
-
-binding.pry
